7206135c25fb1d0d59f03c02e4dd10f8c1a5d6a5
oheart <2353769123@qq.com>
2018-06-08 00:19:00 +0800

    add facelist

--- practice/emoji/src/components/FaceHome.vue
+++ practice/emoji/src/components/FaceHome.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="face-home-container">
+     <!-- 分类 -->
      <div class="category-part">
         <h4 class="mark-tit">分类>></h4>
         <ul class="category-list">
@@ -8,18 +9,22 @@
           <li class="category-item cursor">猫1</li>
           <li class="category-item cursor">猫2</li>
           <li class="category-item cursor">猫3</li> -->
-          <li class="category-item cursor">全部</li>
-          <li 
+          <li class="category-item cursor"
+              :class="{'active-face-item': currentCategoryIndex == null}"
+              @click="clickAllCategoryTab()"
+            >全部</li>
+          <li
               class="category-item cursor"
-              v-for="(item, index) in uniqueCategory" 
+              v-for="(item, index) in uniqueCategory"
               :key="item.fileName"
-              @click="toggleTab(index)"  
-              :class="{'active-face-item': currentIndex == index}"     
+              @click="toggleCategoryTab(index, item)"
+              :class="{'active-face-item': currentCategoryIndex == index}"
             >
             {{item}}
           </li>
         </ul>
      </div>
+     <!-- 标签 -->
      <div class="tag-part">
        <h4 class="mark-tit">标签>></h4>
        <ul class="tag-list">
@@ -28,12 +33,35 @@
           <li class="tag-item cursor">大佬</li>
           <li class="tag-item cursor">大佬</li>
           <li class="tag-item cursor">大佬</li> -->
-          <li class="tag-item cursor">全部</li> 
-         <li class="tag-item cursor" v-for="item in uniqueTags" :key="item.fileName">
+         <li class="tag-item cursor"
+            :class="{'active-face-item': currentTabIndex == null}"
+            @click="clickAllTagTab()"
+            >全部</li>
+         <li class="tag-item cursor"
+              v-for="(item, index) in uniqueTags"
+              :key="item.fileName"
+              @click="toggleTagsTab(index, item)"
+              :class="{'active-face-item': currentTabIndex == index}"
+            >
            {{item}}
          </li>
        </ul>
      </div>
+      <!-- 筛选结果 -->
+      <div class="face-result-part">
+        筛选结果：共 {{totalImgNum}} 张，共 17 页，当前第 1 页。
+      </div>
+     <!-- 对应的表情图片列表 -->
+     <div class="facelist-part">
+       <div class="facelist-item"
+          v-for="item in facesToShow"
+          :key="item.fileName"
+          >
+          <img :src="'../static/' + item.fileName" class="face-img" />
+          <h5 class="face-name">{{item.name}}</h5>
+       </div>
+     </div>
+
   </div>
 </template>
 
@@ -49,7 +77,10 @@ export default {
     return {
       msg: "Welcome to Your Vue.js App",
       faces: face,
-      currentIndex: 0
+      currentCategoryIndex: null,
+      currentTabIndex: null,
+      totalImgNum: 0,
+      facesToShow: face
     };
   },
   computed: {
@@ -63,12 +94,33 @@ export default {
     }
   },
   methods: {
-    toggleTab(index) {
-      this.currentIndex = index;
+    toggleCategoryTab(index, category) {
+      this.currentCategoryIndex = index;
+      // 根据分类过滤要显示的表情图
+      if (category) {
+        this.facesToShow = this.faces.filter(item => item.category == category);
+      }
+    },
+    toggleTagsTab(index, tag) {
+      this.currentTabIndex = index;
+      // 根据分类过滤要显示的表情图
+      if (tag) {
+        this.facesToShow = this.faces.filter(item => item.tag == tag);
+      }
+    },
+    clickAllCategoryTab() {
+      this.currentCategoryIndex = null;
+      this.facesToShow = this.faces.filter(item => item.category != "");
+    },
+    clickAllTagTab() {
+      this.currentTabIndex = null;
+      this.facesToShow = this.faces.filter(item => item.tag != "");
     }
   },
   created() {
     console.log("faces", this.faces);
+    // 表情图片总张数
+    this.totalImgNum = this.faces.length;
   }
 };
 </script>
@@ -125,6 +177,27 @@ export default {
       }
     }
     margin-bottom: 20px;
+  }
+  .facelist-part {
+    display: flex;
+    display: -webkit-flex;
+    flex-wrap: wrap;
+    margin: 0 10%;
+    .facelist-item {
+      width: 20%;
+      text-align: center;
+      margin-bottom: 10px;
+      .face-img {
+        width: 70%;
+      }
+      .face-name {
+        font-size: 18px;
+      }
+    }
+  }
+  .face-result-part {
+    text-align: center;
+    margin: 20px 0;
   }
 }
 </style>
